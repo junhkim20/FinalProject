0c5c38dd0418f00fa206dfd10502fc0cf058e717
junhkim20 <jk7048@nyu.edu>
2023-04-23 22:58:29 -0400

    facts added

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// {
-// 	"factTitle": "",
-// 	"fact": "",
-// 	"source": ""
-// }
-
 let factsJSON = [
 	{
 		"factTitle": "1,300 hours",
@@ -14,16 +8,41 @@ let factsJSON = [
 		"factTitle": "72%",
 		"fact": "This is the percentage of Americans on social media.",
 		"source": "Pew Research Center"
+	}, 
+	{
+		"factTitle": "40%",
+		"fact": "This is the percentage of social media users who get cyber bullied everyday",
+		"source": "Cal Underground"
+	},
+	{
+		"factTitle": "75%",
+		"fact": "This is the percentage of American teens involved in self-harm/suicide in 2021 due to social media",
+		"source": "Fox Business"
+	},
+	{
+		"factTitle": "7% and 20%",
+		"fact": "This is the percentage of increase in severe depression and anxiety disorder with social media usage",
+		"source": "MIT Management Sloan School"
 	}
+
 ];
 
 function mainDisplay() {
 	let mainSection = document.getElementById('mainSection');
 
+	let counter = 0; 
+
 	for(let eachFact of factsJSON) {
 		let factSection = document.createElement('div');
-		
-		factSection.className = 'fact'; 
+		if(counter % 2 == 0){
+			factSection.className = 'fact1'; 
+		} else {
+			factSection.className = 'fact2'
+		}
+		factSection.setAttribute('data-aos', 'fade-up');
+		factSection.setAttribute('data-aos-duration', '3000');
+
+		counter += 1; 
 
 		let factTitle = document.createElement('h1');
 		factTitle.className = 'title';
@@ -34,7 +53,7 @@ function mainDisplay() {
 		factFact.textContent = eachFact.fact;
 	    factSection.appendChild(factFact);
 
-	    let factSource = document.createElement('h2');
+	    let factSource = document.createElement('h5');
 		factSource.textContent = "Source: " + eachFact.source;
 	    factSection.appendChild(factSource);
 
@@ -43,3 +62,4 @@ function mainDisplay() {
 }
 
 mainDisplay(); 
+AOS.init();
